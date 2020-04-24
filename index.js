efe004dec12377cd5f2f47d7e96fa1924e183da2
Jared S <jaredjames2020@gmail.com>
2020-04-24 14:04:44 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 // Your code here
 function mapToNegativize(array){
   numbers = [];
-  array.forEach{function(num){
-    num * -1
-  }
+  
+  array.forEach(function(num){
+    numbers.push(num * -1);
+  });
 };
